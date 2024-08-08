60af784bc2de859c46a872d963ae36e9f9c54f24
Sacha Clerc-Renaud <sachacr@gmail.com>
2024-08-08 20:36:28 +0200

    Fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: "https://sachacr.github.io/hecten-courses/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/hecten-courses/",
+  baseUrl: "/hecten/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
